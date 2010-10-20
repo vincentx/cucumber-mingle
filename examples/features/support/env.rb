e728126a11dc6cac9d165f64c991ba0c23cca044
vincentx <vincent.hao.xu@gmail.com>
2010-10-20 23:47:11 +0800

    add demo mingle project

--- examples/features/support/env.rb
+++ examples/features/support/env.rb
@@ -2,12 +2,13 @@ require 'cucumber/mingle'
 
 AfterConfiguration do |config|
   Cucumber::Mingle.features config, :host => 'http://localhost:8080',  :user => 'vincentx', 
-                           :password => 'password',  :project => 'test'
+                           :password => 'password',  :project => 'calculator', :filters => 'Type IS Story'
   Cucumber::Mingle.results do |invalid_cards, passed, failed, pending|
     count = passed.length + failed.length + pending.length
     test_report = Cucumber::Mingle::Card.new 
     test_report.name = "#{Time.now}"
     test_report.type = 'Test Report'
+    test_report.description = ''
     test_report.save
     test_report = Cucumber::Mingle::Card.find_by_number(test_report.id)   
     test_report['Scenario Count'] = count
